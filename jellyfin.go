20b291d9049c2f428e0c6640081310a6c7df61e3
snowmoe <317636024+snowmoe@users.noreply.github.com>
2026-06-21 14:13:08 +0100

    refactor jellyfin stuff

---
+++ jellyfin.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// /Session endpoint json structure
+// https://api.jellyfin.org/#tag/Session/operation/GetSessions
+type Session struct {
+	UserName       string `json:"UserName"`
+	NowPlayingItem `json:"NowPlayingItem"`
+	PlayState      struct {
+		IsPaused      bool  `json:"IsPaused"`
+		PositionTicks int64 `json:"PositionTicks"`
+	} `json:"PlayState"`
+}
+
+type NowPlayingItem struct {
+	Name              string `json:"Name"`
+	Id                string `json:"Id"`
+	Type              string `json:"Type"`
+	RunTimeTicks      int64  `json:"RunTimeTicks"`
+	SeriesName        string `json:"SeriesName,omitempty"`
+	SeriesId          string `json:"SeriesId,omitempty"`
+	ParentIndexNumber int    `json:"ParentIndexNumber,omitempty"`
+	IndexNumber       int    `json:"IndexNumber,omitempty"`
+}
+
+// very simple functon compared to the other ipc shit
+// just call the GET /Sessions endpoint and unmarshal into our structs
+// making sure we only get the session for the specified user
+func getJellyfinSessions(cfg *Config) (*Session, error) {
+	req, _ := http.NewRequest("GET", cfg.JellyfinURL+"/Sessions", nil)
+	req.Header.Set("X-MediaBrowser-Token", cfg.JellyfinToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var sessions []Session
+	if err := json.NewDecoder(resp.Body).Decode(&sessions); err != nil {
+		return nil, err
+	}
+
+	for _, s := range sessions {
+		if s.UserName == cfg.JellyfinUser && s.NowPlayingItem.Name != "" {
+			return &s, nil
+		}
+	}
+	return &Session{}, nil
+}
+
+func isSessionActive(sess *Session) bool {
+	if sess == nil {
+		return false
+	}
+
+	if sess.NowPlayingItem.Name == "" || sess.NowPlayingItem.Id == "" {
+		return false
+	}
+
+	return true
+}
--- main.go
+++ main.go
@@ -1,37 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 	"time"
 )
 
 // TODO add optional config opt to override the app id
 const applicationID = "1517892834907394229"
-
-// /Session endpoint json structure
-// https://api.jellyfin.org/#tag/Session/operation/GetSessions
-type Session struct {
-	UserName       string `json:"UserName"`
-	NowPlayingItem `json:"NowPlayingItem"`
-	PlayState      struct {
-		IsPaused      bool  `json:"IsPaused"`
-		PositionTicks int64 `json:"PositionTicks"`
-	} `json:"PlayState"`
-}
-
-type NowPlayingItem struct {
-	Name              string `json:"Name"`
-	Id                string `json:"Id"`
-	Type              string `json:"Type"`
-	RunTimeTicks      int64  `json:"RunTimeTicks"`
-	SeriesName        string `json:"SeriesName,omitempty"`
-	SeriesId          string `json:"SeriesId,omitempty"`
-	ParentIndexNumber int    `json:"ParentIndexNumber,omitempty"`
-	IndexNumber       int    `json:"IndexNumber,omitempty"`
-}
 
 func main() {
 	configDir, err := os.UserConfigDir()
@@ -126,8 +102,6 @@ func main() {
 			endEpoch := now + (remainingSec * 1000)
 
 			// then we set our activity status using the current playing item + epochs we calculated
-			// TODO fetch the status correctly (e.g. EP no + ep name for series, and/or something adhoc for movies)
-			// TODO fetch and add cover art for media
 			err = dc.SetWatching(rpcTitle, rpcState, artworkURL, startEpoch, endEpoch)
 			if err != nil {
 				fmt.Println(err)
@@ -135,42 +109,4 @@ func main() {
 			}
 		}
 	}
-}
-
-// very simple functon compared to the other ipc shit
-// just call the GET /Sessions endpoint and unmarshal into our structs
-// making sure we only get the session for the specified user
-func getJellyfinSessions(cfg *Config) (*Session, error) {
-	req, _ := http.NewRequest("GET", cfg.JellyfinURL+"/Sessions", nil)
-	req.Header.Set("X-MediaBrowser-Token", cfg.JellyfinToken)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var sessions []Session
-	if err := json.NewDecoder(resp.Body).Decode(&sessions); err != nil {
-		return nil, err
-	}
-
-	for _, s := range sessions {
-		if s.UserName == cfg.JellyfinUser && s.NowPlayingItem.Name != "" {
-			return &s, nil
-		}
-	}
-	return &Session{}, nil
-}
-
-func isSessionActive(sess *Session) bool {
-	if sess == nil {
-		return false
-	}
-
-	if sess.NowPlayingItem.Name == "" || sess.NowPlayingItem.Id == "" {
-		return false
-	}
-
-	return true
 }
